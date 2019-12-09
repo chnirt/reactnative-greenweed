4e20c5f2d33fc0b2695e865fd876736a7da1f553
chin trinh <trinhchinchin@gmail.com>
2019-12-09 12:01:52 +0700

    Fix eslint + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+	root: true,
+	extends: '@react-native-community',
+	rules: {
+		indent: ['error', 2],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+		'sort-keys': ['error', 'asc', {caseSensitive: true, natural: false}],
+		'comma-dangle': ['error', 'never']
+	}
+}
